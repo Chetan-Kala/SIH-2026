c71aa7569a013de4edae70e46d24ef399a165fd6
Chetan Kala <chetankala93@gmail.com>
2026-09-10 21:36:21 +0530

    fix: standard prisma client for vercel

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,7 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 import 'dotenv/config'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient()
 
 async function main() {
   await prisma.district.createMany({
